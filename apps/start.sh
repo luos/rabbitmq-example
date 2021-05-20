d41ba530d5750ac00541a6d01f508209a3c2e711
Lajos Gerecs <lajos.gerecs@erlang-solutions.com>
2021-05-20 12:01:30 +0200

    cleanup code and set shovel prefetch

--- apps/start.sh
+++ apps/start.sh
@@ -1,18 +1,11 @@
 #/bin/bash
 set -ex
-LOCK=/apps/lock
 
-# hack so the two compiles dont disturb each other
-sleep .$[ ( $RANDOM % 1000 ) + 1 ]s
-while test -f "$LOCK"; do
-    sleep 5
-done
-
-touch $LOCK
-pushd /apps/simulator
+mkdir -p /simulator
+cp -rf /apps/simulator/* /simulator/
+ls -alh /simulator
+pushd /simulator
 mix local.hex --force
 mix local.rebar --force
 mix deps.compile
-rm $LOCK || true
-sleep 5
 mix run  lib/simulate.exs  --host $RABBITMQ_HOST  --port 5672 --instance-name $INSTANCE_NAME
